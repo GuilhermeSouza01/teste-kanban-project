008c14b36261f3aa1ace85aad2481e50a2334a48
GuilhermeSouza01 <ghostgui60@gmail.com>
2024-08-26 17:32:33 -0300

    feat: Creating the requests responsible for update column and tasks

---
+++ backend/app/Http/Requests/UpdateColumnRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateColumnRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'header_background_color' => ['nullable', 'string', 'max:7'],
+            'position' => ['integer', 'min:0']
+        ];
+    }
+}
