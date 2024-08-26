64f285ba7385e04ffb96f617e166682961866c17
GuilhermeSouza01 <ghostgui60@gmail.com>
2024-08-26 17:35:53 -0300

    feat: Creating controllers for manipulating api data

---
+++ backend/app/Http/Controllers/ColumnController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreColumnRequest;
+use App\Http\Requests\UpdateColumnRequest;
+use App\Models\Column;
+use Illuminate\Http\Request;
+
+class ColumnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $columns = Column::get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Colunas listadas com sucesso',
+            'data' => $columns
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreColumnRequest $request)
+    {
+        $request->validated();
+
+        $column = Column::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Coluna criada com sucesso',
+            'data' => $column
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Column $column)
+    {
+        $column = $column->load('tasks');
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Coluna listada com sucesso',
+            'data' => $column
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateColumnRequest $request, string $id)
+    {
+        $request->validated();
+
+        $column = Column::findOrFail($id);
+        $column->update($request->only(['title', 'header_background_color', 'position']));
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Coluna atualizada com sucesso',
+            'data' => $column
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Column $column)
+    {
+        $column->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Coluna deletada com sucesso',
+            'data' => $column
+        ], 200);
+    }
+
+    /**
+     * Order the columns.
+     */
+    public function reorderColumn(UpdateColumnRequest $request)
+    {
+
+        $request->validated();
+
+        $columns = $request->input('columns');
+
+        //Atualiza a ordem das colunas
+
+        // Atualiza a ordem das colunas
+        foreach ($columns as $index => $column) {
+            // Atualiza a ordem da coluna baseada na posição do array
+            Column::where('id', $column['id'])->update(['order' => $index]);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Colunas reordenadas com sucesso'
+        ], 200);
+    }
+}
---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::with('column')->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefas listadas com sucesso',
+            'data' => $tasks
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $request->validated();
+
+        $task = Task::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefa criada com sucesso',
+            'data' => $task
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefa listada com sucesso',
+            'data' => $task
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, string $id)
+    {
+        $request->validated();
+
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefa atualizada com sucesso',
+            'data' => $task
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefa deletada com sucesso'
+        ], 200);
+    }
+
+    public function reorderTasks(UpdateTaskRequest $request)
+    {
+        $request->validated();
+
+        $tasks = $request->input('tasks');
+
+        foreach ($tasks as $task) {
+            Task::where('id', $task['id'])->update([
+                'order' => $task['order'],
+                'column_id' => $task['column_id']
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tarefas reordenadas com sucesso'
+        ], 200);
+    }
+}
