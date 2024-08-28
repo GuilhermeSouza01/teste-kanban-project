9b7d71640ca0536949080613b881227d1091de7e
Guilherme Souza <ghostgui60@gmail.com>
2024-08-28 13:01:01 -0300

    feat: Publish observers on provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Column;
+use App\Models\Task;
+use App\Observers\ColumnObserver;
+use App\Observers\TaskObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Task::observe(TaskObserver::class);
+        Column::observe(ColumnObserver::class);
     }
 }
