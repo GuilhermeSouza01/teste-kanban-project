e551d7e23053b22a3548ec814e23ea6505c0d26e
Guilherme Souza <ghostgui60@gmail.com>
2024-08-28 22:47:54 -0300

    fix: Remove observers

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -23,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Task::observe(TaskObserver::class);
-        Column::observe(ColumnObserver::class);
+        //
     }
 }
