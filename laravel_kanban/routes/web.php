1a072331bc840ee0efc64ba9a441c67f21e2f03f
Guilherme Souza <ghostgui60@gmail.com>
2024-08-31 17:02:50 -0300

    feat:Redirect principal route to /board

--- laravel_kanban/routes/web.php
+++ laravel_kanban/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('board.index');
 });
 
 Route::get('/board', [BoardController::class, 'showBoardIndex'])->name('board.index');
